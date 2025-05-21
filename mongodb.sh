37184f12e87bc7bcbaa67f76f7a9e226d3d03114
unknown <harha@gmail.com>
2025-05-21 21:50:28 +0530

    f

--- mongodb.sh
+++ mongodb.sh
@@ -1,51 +1,52 @@
 #!/bin/bash
 
+# Variables
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
-N="\e[0m
-LOGS_FOLDER="var/log/roboshop-logs"
+N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
-mkdir -p $LOGS_FOLDERS
-echo "Script started executeing at: $" | tee -a $LOG_FILE
+# Create log directory
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
-#CHECK THE USER HAS ROOT PRIVELEGES OR NOT 
-if [ $USERID -ne 0 ]
-then
-    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG _FILE
-    exit 1 #give other than 0 upto 127
-else 
+# Check root access
+if [ $USERID -ne 0 ]; then
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1
+else
     echo "You are running with root access" | tee -a $LOG_FILE
 fi
 
-#validate functions takes input as exit status, what command they tried to install
+# Validation function
 VALIDATE(){
-    if [ $1 -eq 0]
-    then 
-        echo -e "$2 is ...$G SUCESS $N" | tee -a $LOG_FILE
+    if [ $1 -eq 0 ]; then
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
     else
-        echo -e "$2 is ...$R FAILURE $N" | tee -a $LOG_FILE
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
         exit 1
     fi
 }
 
+# MongoDB setup
 cp mongo.repo /etc/yum.repos.d/mongodb.repo
 VALIDATE $? "Copying MongoDB repo"
 
-dnf install mongod-org -y &>>$LOG_FILE
-VALIDATE $? "Installing mongodb server"
+dnf install mongodb-org -y &>>$LOG_FILE
+VALIDATE $? "Installing MongoDB server"
 
 systemctl enable mongod &>>$LOG_FILE
-VALIDATE $? Installing ongodb server
+VALIDATE $? "Enabling MongoDB service"
 
 systemctl start mongod &>>$LOG_FILE
-VALIDATE $? "Starting MongoDB"
+VALIDATE $? "Starting MongoDB service"
 
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/monogd.conf
-VALIDATE $? "Editing MongoDB conf file for remote connections"
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "Editing MongoDB config for remote access"
 
-systemcl restart mongod &>>$LOG_FILE
-VALIDATE $? Restarting MongoDB
+systemctl restart mongod &>>$LOG_FILE
+VALIDATE $? "Restarting MongoDB service"
