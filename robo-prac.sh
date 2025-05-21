ad377219eea95f8d0ca67fa0ad3243d97acbf3a7
unknown <harha@gmail.com>
2025-05-21 16:10:34 +0530

    f

--- robo-prac.sh
+++ robo-prac.sh
@@ -13,7 +13,7 @@ do
 
     INSTANCE_ID=$(aws ec2 run-instances \
         --image-id $AMI_ID \
-        --instance-type t3.micro \
+        --instance-type t2.micro \
         --security-group-ids $SG_ID \
         --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" \
         --query "Instances[0].InstanceId" \
